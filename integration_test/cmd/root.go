31ae209e27087431c4f8662bd08dff354888460b
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-05-04 20:44:49 +0200

    Added another command repo-ssh-key

--- integration_test/cmd/root.go
+++ integration_test/cmd/root.go
@@ -16,20 +16,23 @@ var (
 		platform  string
 		eventType string
 		payload   string
+		sshKey    string
 	}
 )
 
 func init() {
 	flag := rootCmd.Flags()
 	// Server Configs
-	flag.StringVar(&rootArgs.platform, "platform", "", "--platform github")
+	flag.StringVar(&rootArgs.platform, "platform", "", "--platform github.")
 	flag.StringVar(&rootArgs.eventType, "event-type", "", "--event-type The type of the event occurred.")
-	flag.StringVar(&rootArgs.eventType, "payload", "", "--payload The payload.")
+	flag.StringVar(&rootArgs.payload, "payload", "", "--payload The payload.")
+	flag.StringVar(&rootArgs.sshKey, "repo-ssh-key", "", "--repo-ssh-key Auth information for the repo checkout.")
+
 }
 
 // runRootCmd runs the main notifier command.
 func runRootCmd(cmd *cobra.Command, args []string) {
-	fmt.Printf("platform: %s,event-type: %s,payload: %s", rootArgs.platform, rootArgs.eventType, rootArgs.payload)
+	fmt.Printf("platform: %s,event-type: %s,payload: %s,repo-ssh-key: %s", rootArgs.platform, rootArgs.eventType, rootArgs.payload, rootArgs.sshKey)
 }
 
 // Execute runs the main krok command.
